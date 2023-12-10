df6a7ad733a4c86c83a3195fe7f03ee98dd2b496
TedJenkler <Tedjenkler@gmail.com>
2023-12-10 04:47:10 -0200

    productlink hompage mobile added

--- src/Pages.js/HomePage.js
+++ src/Pages.js/HomePage.js
@@ -3,6 +3,8 @@ import Nav from "../Components.js/Nav"
 import headphones from "../Img/headphones.png"
 import speaker from "../Img/speakers.png"
 import earphones from "../Img/earphones.png"
+import speakerx200 from "../Img/speaker-200px.png"
+import earphoneimg from "../Img/earphonesimg.png"
 
 const HomePage = () => {
     return (
@@ -22,6 +24,29 @@ const HomePage = () => {
                 <LinkItem img={speaker} h2="SPEAKERS" />
                 <LinkItem img={earphones} h2="EARPHONES" />
             </section>
+            <div className="flex flex-col items-center">
+            <section className="bg-darkorange w-11/12 flex flex-col items-center text-center pt-12 rounded-lg pb-14 mb-6 circlebg bg-top bg-no-repeat">
+                <img className="mb-8" src={speakerx200} alt="speaker" />
+                <h1 className="mb-6 text-4xl text-white">ZX9 <br/> SPEAKER</h1>
+                <p className="mb-6 mx-6 text-white text-base">Upgrade to premium speakers that are phenomenally built to deliver truly remarkable sound.</p>
+                <button className="py-3 px-8 text-white bg-black2">SEE PRODUCT</button>
+            </section>
+            </div>
+            <div className="flex flex-col items-center">
+                <section className="py-24 bg-greywhite w-11/12 speakerbg bg-right bg-no-repeat bg-cover mb-6">
+                    <h1 className="ml-6 mb-8">ZX7 SPEAKER</h1>
+                    <button className="ml-6 border-black2 border py-3 px-8">SEE PRODUCT</button>
+                </section>
+            </div>
+            <div className="flex flex-col items-center">
+                <section className="w-11/12 mb-32">
+                    <img className="w-full mb-6" src={earphoneimg} alt="Earphones" />
+                    <div className="bg-greywhite py-10">
+                        <h1 className="ml-6 mb-8">ZX7 SPEAKER</h1>
+                        <button className="ml-6 border-black2 border py-3 px-8">SEE PRODUCT</button>
+                    </div>
+                </section>
+            </div>
         </main>
         </>
     )
