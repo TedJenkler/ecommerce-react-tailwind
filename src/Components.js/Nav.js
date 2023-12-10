3470a92899c3c76612a7b57509d5e53b67a6499e
TedJenkler <Tedjenkler@gmail.com>
2023-12-10 00:17:15 -0200

    tailwind added

---
+++ src/Components.js/Nav.js
@@ -0,0 +1,11 @@
+const Nav = () => {
+    return (
+        <>
+        <nav className="pt-8">
+            
+        </nav>
+        </>
+    )
+}
+
+export default Nav
---
+++ src/Pages.js/HomePage.js
@@ -0,0 +1,13 @@
+import Nav from "../Components.js/Nav"
+
+const HomePage = () => {
+    return (
+        <>
+        <header>
+            <Nav />
+        </header>
+        </>
+    )
+}
+
+export default HomePage
