54ea7e854135dded501290a71bbe5c219fc63558
TedJenkler <Tedjenkler@gmail.com>
2023-12-21 05:51:55 -0200

    checkpoint cart

--- src/Pages.js/Cart.js
+++ src/Pages.js/Cart.js
@@ -12,11 +12,16 @@ const Cart = () => {
     const render = () => {
         return(
         Object.values(state.cart).map((items) => {
-            return (<div className="flex mb-6 gap-4">
+            return (<div className="flex mb-6 gap-4 relative">
                 <img className="h-16 w-16 rounded-lg" src={items.img} alt={items.product} />
                 <div className="flex flex-col justify-around">
                     <h1 className="text-base font-bold">{items.product}</h1>
                     <p className="text-bordergrey text-sm">$ {items.cost}</p>
+                </div>
+                <div className="flex items-center absolute right-0 top-1/2 bottom-1/2">
+                    <button className="h-6 w-6 bg-greywhite flex items-center justify-center">-</button>
+                    <p className="h-6 w-6 bg-greywhite flex items-center justify-center">{items.amount}</p>
+                    <button className="h-6 w-6 bg-greywhite flex items-center justify-center">+</button>
                 </div>
                 </div>)
         }))
