b13b5883b58ee2c39d61c91793ddecc79c13537f
TedJenkler <Tedjenkler@gmail.com>
2023-12-22 06:38:17 -0200

    responsive mobile and tablet - some styling fixes needed like img and font styling

--- src/Components.js/Menu.js
+++ src/Components.js/Menu.js
@@ -8,8 +8,8 @@ import { useContext} from "react";
 const Menu = () => {
     const [state, dispatch] = useContext(Context)
     return (
-        <section className="relative">
-        <nav className={state.togglemenu === true ? "hidden absolute w-full bg-white z-50 flex-col items-center" : "flex absolute w-full bg-white z-50 flex-col items-center"}>
+        <section className="relative min-w-full">
+        <nav className={state.togglemenu === true ? "hidden" : "flex absolute w-full bg-white z-50 flex-col items-center md:flex-row md:gap-2 md:mb-24 md:mx-0 md:px-10 md:py-12"}>
             <LinkItem img={headphones} h2="HEADPHONES" link="/headphones" />
             <LinkItem img={speaker} h2="SPEAKERS" link="/speakers" />
             <LinkItem img={earphones} h2="EARPHONES" link={"/earphones"} />
