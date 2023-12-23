102661a3654d3c87e2ed2acb4897461bce2aaaee
TedJenkler <Tedjenkler@gmail.com>
2023-12-23 05:16:52 -0200

    gh pages checkpoint responsive on mobile, tablet and lg but with alot of styling errors

--- src/Components.js/Conformation.js
+++ src/Components.js/Conformation.js
@@ -11,7 +11,7 @@ const Conformation = ( {toggle} ) => {
         dispatch({type: 'totalbeforetax', payload: 0})
     }
     return (
-        <section className={toggle === false ? " bg-white rounded-lg w-11/12 p-8 pb-16 absolute left-0 right-0 mx-auto top-56 z-50 md:w-3/4" : "hidden bg-white rounded-lg w-11/12 p-8 pb-16 absolute left-0 right-0 mx-auto top-56"}>
+        <section className={toggle === false ? " bg-white rounded-lg w-11/12 p-8 pb-16 absolute left-0 right-0 mx-auto top-56 z-50 md:w-3/4 lg:w-1/2 xl:w-1/3" : "hidden bg-white rounded-lg w-11/12 p-8 pb-16 absolute left-0 right-0 mx-auto top-56"}>
             <div className="bg-darkorange h-16 w-16 flex items-center justify-center rounded-full mb-6">
                 <img className="h-5 w-5" src={check} alt="oval"></img>
             </div>
@@ -19,8 +19,8 @@ const Conformation = ( {toggle} ) => {
             <h1 className="text-2xl font-bold mb-4">FOR YOUR ORDER</h1>
             <p className="text-base text-bordergrey mb-6">You will receive an email confirmation shortly.</p>
             <div className="md:flex md:h-36 md:mb-24 md:w-full">
-                <div className="md:flex md:flex-col">
-                    <div className="border-b border-bordergrey bg-greywhite rounded-t-lg p-6 md:rounded-tl-lg md:rounded-tr-none">
+                <div className="md:flex md:flex-col lg:w-2/3">
+                    <div className="border-b border-bordergrey bg-greywhite rounded-t-lg p-6 md:rounded-tl-lg md:rounded-tr-none lg:px-4">
                         <div className="flex mb-6 gap-4">
                         <img className="h-16 w-16 rounded-lg" src={Object.values(state.cart)[0].img} alt={Object.values(state.cart)[0].product} />
                         <div className="flex flex-col justify-around">
@@ -34,7 +34,7 @@ const Conformation = ( {toggle} ) => {
                     </div>
                     {state.amount > 1 ? <div className="flex text-center bg-greywhite pb-6 pt-3 md:pb-9 md:rounded-bl-lg"><p className="w-full text-bordergrey">and {state.amount - 1} other item{state.amount > 2 ? "(s)" : null}</p></div> : null}
                 </div>
-                <div className="bg-black2 pt-4 px-6 pb-5 mb-6 rounded-b-lg md:rounded-r-lg md:rounded-l-none md:h-52 md:flex md:flex-col md:justify-center md:w-1/2">
+                <div className="bg-black2 pt-4 px-6 pb-5 mb-6 rounded-b-lg md:rounded-r-lg md:rounded-l-none md:h-52 md:flex md:flex-col md:justify-center md:w-1/2 lg:w-1/3">
                     <h1 className="text-bordergrey text-base mb-2">GRAND TOTAL</h1>
                     <p className="text-white font-bold text-lg">$ {state.total + 50}</p>
                 </div>
